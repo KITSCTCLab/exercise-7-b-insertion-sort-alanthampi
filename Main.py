910498e1ee6193b81030dbc2881141941cba3ed7
alanthampi <109643783+alanthampi@users.noreply.github.com>
2022-10-12 09:48:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
 from typing import List
 
-def insertionSort(array) -> List[int]:
-  # Write your code here
+def insertionSort(array,size) -> List[int]:
+  for i in range(size-1):
+      key = array[i]
+      j = size-1
+      while(j>=0 and a[j]>key):
+          list[j+1] = list[j]
+          j = j-1
+          list[j+1] = key
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
